93a51a53516d5dfaa490bd1471ebb08d1bc0c521
Blake Trebelhorn <blake.trebelhorn@gmail.com>
2017-01-05 11:39:00 -0600

    Started checkIfWon function

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,3 +1,23 @@
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+
+def checkIfWon(board):
+    # check top row
+    if board['top-L'] == board['top-M'] == board['top-R']:
+        return True
+    # check mid row
+    elif board['mid-L'] == board['mid-M'] == board['mid-R']:
+        return True
+    # check low row
+    elif board['low-L'] == board['low-M'] == board['low-R']:
+        return True

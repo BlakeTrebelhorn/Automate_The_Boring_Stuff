cf032325318dbb98506239246fe03d5bbc338802
Blake Trebelhorn <Blake Trebelhorn>
2017-01-05 10:43:55 -0600

    Chapter 1 completed

---
+++ Part1_Chapter1.py
@@ -0,0 +1,11 @@
+print('Hello world!')
+print('What\'s your name?')
+name = input()
+print('It\'s good to meet you', name + '!')
+print('The length of your name is:', len(name))
+print('What\'s your age?')
+age = input()
+print('You will be ' + str(int(age) + 1) + ' in a year.')
+input()
+# I added this because I'm using sublime text and running
+# straight from the windows explorer
